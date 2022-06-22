c08449d16a56f8bc9e301580d3b76307752441cf
rafaelpinheiro84 <rafaelsp0584@gmail.com>
2022-06-22 17:28:12 -0300

    UC14 Encontro1 Api

---
+++ ChapterFST1/Controllers/LivroController.cs
@@ -0,0 +1,34 @@
+﻿using ChapterFST1.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChapterFST1.Controllers
+{
+    [Produces("application/json")]
+
+    [Route("api/[controller]")]
+
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        private readonly LivroRepository _livroRepository;
+
+        public LivroController(LivroRepository livroRepository)
+        {
+            _livroRepository = livroRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Lista()
+        {
+            try
+            {
+              return Ok( _livroRepository.Listar());
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
---
+++ ChapterFST1/Repositories/LivroRepository.cs
@@ -0,0 +1,20 @@
+﻿using ChapterFST1.Contexts;
+using ChapterFST1.Models;
+
+namespace ChapterFST1.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly ChapterContext _context;
+
+        public LivroRepository (ChapterContext context) 
+        {
+            _context = context;
+        }
+
+        public List<Livro> Listar() 
+        {
+            return _context.Livros.ToList();
+        }
+    }
+}
