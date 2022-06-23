e47c0e17108b7b548878f3ca3e9133a9f3efdea5
rafaelpinheiro84 <rafaelsp0584@gmail.com>
2022-06-23 19:50:06 -0300

    UC12 Encontro2 api

--- ChapterFST1/Controllers/LivroController.cs
+++ ChapterFST1/Controllers/LivroController.cs
@@ -1,4 +1,5 @@
-﻿using ChapterFST1.Repositories;
+﻿using ChapterFST1.Models;
+using ChapterFST1.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -28,6 +29,81 @@ namespace ChapterFST1.Controllers
             catch (Exception e)
             {
                 throw new Exception(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+
+                Livro livro = _livroRepository.BuscarPorId(id);
+
+                if (livro == null)
+                {
+                    return NotFound();
+                } 
+
+                return Ok(livro);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult Cadastrar(Livro livro)
+        {
+            try
+            {
+                _livroRepository.Cadastrar(livro);
+
+                return StatusCode(201);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id,Livro livro)
+        {
+            try
+            {
+                _livroRepository.Atualizar(id, livro);
+
+                return StatusCode(204);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _livroRepository.Deletar(id);
+
+                return StatusCode(204);
+            }
+            catch (Exception)
+            {
+
+                throw;
             }
         }
     }
